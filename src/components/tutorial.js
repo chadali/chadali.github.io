805dc147c3df9d99dd92035c70cb1cc65df505f9
chadali <23alic@gmail.com>
2018-06-26 23:04:48 -0400

    React Routing - Hash

---
+++ src/components/tutorial.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Tutorial extends Component {
+  render() {
+    return (
+      <div>
+        test
+      </div>
+    );  
+  }
+}
+
+export default Tutorial;
