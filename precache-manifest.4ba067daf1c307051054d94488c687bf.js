8d8734b0e648e765f6a78de84629aa1d215e7946
chadali <23alic@gmail.com>
2019-01-01 06:16:14 -0500

    Updates

---
+++ precache-manifest.4ba067daf1c307051054d94488c687bf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "03195697b11eafb7a42ae6e1b0ef262b",
+    "url": "/static/media/swift.03195697.png"
+  },
+  {
+    "revision": "c8f9fb082e5f80c52ee9",
+    "url": "/static/css/main.97f8dabf.chunk.css"
+  },
+  {
+    "revision": "0ffcdff6d378c1c2cc5a",
+    "url": "/static/js/1.0ffcdff6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "95be5582c88e9af53226bf135ec95bf7",
+    "url": "/static/media/book.95be5582.jpeg"
+  },
+  {
+    "revision": "7bd1e3832ef2b5b69195e7ea5ec4a201",
+    "url": "/static/media/code.7bd1e383.png"
+  },
+  {
+    "revision": "3f462af1f460d291d930794fdf364767",
+    "url": "/static/media/movie.3f462af1.jpg"
+  },
+  {
+    "revision": "f8267b71a21ec798c6bdafe8ba90e234",
+    "url": "/static/media/reactjs.f8267b71.png"
+  },
+  {
+    "revision": "c8f9fb082e5f80c52ee9",
+    "url": "/static/js/main.c8f9fb08.chunk.js"
+  },
+  {
+    "revision": "8363205db16e7c1831917e3b676f0f51",
+    "url": "/static/media/python.8363205d.png"
+  },
+  {
+    "revision": "e30ffe88468133b594a39ad56bea99bf",
+    "url": "/static/media/sql.e30ffe88.png"
+  },
+  {
+    "revision": "3e65df6aa02a0aba3e53ec1770af886e",
+    "url": "/static/media/websocket.3e65df6a.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "8254befa7a62d58a66edb50c09fa18fb",
+    "url": "/static/media/Ali: A Life.8254befa.jpg"
+  },
+  {
+    "revision": "ea54796ae2c58b8d5fd170efe83fe45d",
+    "url": "/static/media/Neuromancer.ea54796a.jpg"
+  },
+  {
+    "revision": "8ff93af5179684da288d34f33eb48dae",
+    "url": "/static/media/spinner.8ff93af5.svg"
+  },
+  {
+    "revision": "0b27eff5deacaa187a3e5eb482fe1653",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,34 @@
-"use strict";var precacheConfig=[["/index.html","c6b625682b30bd3f44e7f813a329d873"],["/static/css/main.978e070f.css","f8d36f75e4aac8b0d61436294be1927f"],["/static/js/main.172621e9.js","2820f71a5678d6476d0061dd2e638744"],["/static/media/Ali: A Life.8254befa.jpg","8254befa7a62d58a66edb50c09fa18fb"],["/static/media/Neuromancer.ea54796a.jpg","ea54796ae2c58b8d5fd170efe83fe45d"],["/static/media/book.95be5582.jpeg","95be5582c88e9af53226bf135ec95bf7"],["/static/media/code.7bd1e383.png","7bd1e3832ef2b5b69195e7ea5ec4a201"],["/static/media/github.76d344f0.svg","76d344f08412543c74ccd1fb2493d607"],["/static/media/movie.3f462af1.jpg","3f462af1f460d291d930794fdf364767"],["/static/media/python.8363205d.png","8363205db16e7c1831917e3b676f0f51"],["/static/media/reactjs.f8267b71.png","f8267b71a21ec798c6bdafe8ba90e234"],["/static/media/spinner.8ff93af5.svg","8ff93af5179684da288d34f33eb48dae"],["/static/media/sql.e30ffe88.png","e30ffe88468133b594a39ad56bea99bf"],["/static/media/swift.03195697.png","03195697b11eafb7a42ae6e1b0ef262b"],["/static/media/websocket.3e65df6a.svg","3e65df6aa02a0aba3e53ec1770af886e"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var a=new URL(e);return"/"===a.pathname.slice(-1)&&(a.pathname+=t),a.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob()).then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,a,n){var r=new URL(e);return n&&r.pathname.match(n)||(r.search+=(r.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(a)),r.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var a=new URL(t).pathname;return e.some(function(e){return a.match(e)})},stripIgnoredUrlParameters=function(e,a){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return a.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],a=e[1],n=new URL(t,self.location),r=createCacheKey(n,hashParamName,a,/\.\w{8}\./);return[n.toString(),r]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(n){return setOfCachedUrls(n).then(function(a){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!a.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return n.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var a=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!a.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,a=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),n="index.html";(e=urlsToCacheKeys.has(a))||(a=addDirectoryIndex(a,n),e=urlsToCacheKeys.has(a));var r="/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(a=new URL(r,self.location).toString(),e=urlsToCacheKeys.has(a)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(a)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.4ba067daf1c307051054d94488c687bf.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
