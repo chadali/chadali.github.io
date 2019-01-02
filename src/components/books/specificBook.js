632a293098e2a078959675743b83bf650604f085
chadali <23alic@gmail.com>
2019-01-01 20:48:57 -0500

    better style and responsive mobile design

--- src/components/books/specificBook.js
+++ src/components/books/specificBook.js
@@ -16,7 +16,7 @@ class SpecificBook extends React.Component {
         <figure className="snip1556">
           <figcaption>
             <h3>{this.props.book.title}</h3>
-          </figcaption><img src={Images['books'][this.props.book.title.trim()]} style={{maxHeight:"300px"}} className="img-rounded img-responsive" alt={alt}/>
+          </figcaption><img src={Images['books'][this.props.book.title.trim()]} className="bookImage img-rounded img-responsive" alt={alt}/>
         </figure>
 
       </span>   
